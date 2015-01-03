7fbf16d219e29524d46f52ac09d7ed21d60c2b81
Deepak Parpyani <dparpyani@yahoo.com>
2015-01-03 14:03:18 -0500

    Add stub for Q31.

---
+++ Q31-Q41 (Arithmetic)/Q31 - Primes.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+;; Question:
+;; Determine whether a given integer number is prime.
+;;   Example:
+;;    (is-prime 7)
+;; => #t
+;;
+
+(define (is-prime x)
+  (error "Not implemented yet"))
