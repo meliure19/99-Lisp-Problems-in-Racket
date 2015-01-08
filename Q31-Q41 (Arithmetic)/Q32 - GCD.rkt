75ae846fe67aa9dd52af470830f1cabc02d801c6
Deepak Parpyani <dparpyani@yahoo.com>
2015-01-07 20:21:08 -0500

    Add Q32.

---
+++ Q31-Q41 (Arithmetic)/Q32 - GCD.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(require typed/racket)
+
+;; Question:
+;; Determine the greatest common divisor of two positive integer numbers. Use Euclid's algorithm.
+;; Example:
+;; (gcd 36 63)
+;; => 9
+;;
+
+(define (gcd a b)
+  (error "Not implemented yet"))
