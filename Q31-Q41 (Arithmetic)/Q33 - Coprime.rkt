e94bb027200478c1944e7c7504a3ee9be4ec282b
Deepak Parpyani <dparpyani@yahoo.com>
2015-01-08 09:21:39 -0500

    Add Q33.

---
+++ Q31-Q41 (Arithmetic)/Q33 - Coprime.rkt
@@ -0,0 +1,13 @@
+#lang racket
+(require typed/racket)
+
+;; Question:
+;; Determine whether two positive integer numbers are coprime.
+;; Two numbers are coprime if their greatest common divisor equals 1.
+;; Example:
+;; (coprime 35 64)
+;; => #t
+;;
+
+(define (coprime a b)
+  (error "Not implemented yet"))
