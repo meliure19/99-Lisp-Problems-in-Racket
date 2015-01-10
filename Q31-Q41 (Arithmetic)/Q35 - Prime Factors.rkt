64c525767d99c2bb25eb2d7c5ec1fa69bdf5f1b7
Deepak Parpyani <dparpyani@yahoo.com>
2015-01-10 08:49:27 -0500

    Add Q35.

---
+++ Q31-Q41 (Arithmetic)/Q35 - Prime Factors.rkt
@@ -0,0 +1,13 @@
+#lang racket
+(require typed/racket)
+
+;; Question:
+;; Determine the prime factors of a given positive integer.
+;; Construct a flat list containing the prime factors in ascending order.
+;; Example:
+;; (prime-factors 315)
+;; => (3 3 5 7)
+;;
+
+(define (prime-factors x)
+  (error "Not implemented yet"))
