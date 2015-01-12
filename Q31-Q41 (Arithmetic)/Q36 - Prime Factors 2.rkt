52e2ef85a3444d39472709f9a9fc3905b8151d79
Deepak Parpyani <dparpyani@yahoo.com>
2015-01-11 20:56:33 -0500

    Add Q36.

---
+++ Q31-Q41 (Arithmetic)/Q36 - Prime Factors 2.rkt
@@ -0,0 +1,13 @@
+#lang racket
+(require typed/racket)
+
+;; Question:
+;; Determine the prime factors of a given positive integer (2).
+;; Construct a list containing the prime factors and their multiplicity.
+;;   Example:
+;;    (prime-factors-mult 315)
+;; => '((3 2) (5 1) (7 1))
+;;
+
+(define (prime-factors-mult x)
+  (error "Not implemented yet"))
